60547f1cc6b165fc4a4c7207c15f030b7107284b
kilozdazolik <paulsonzoltan@protonmail.com>
2025-02-05 15:27:23 +0100

    Add FAQ accordion functionality and SVG icons; implement responsive styles

---
+++ newbie/faq-accordion-main/script.js
@@ -0,0 +1,10 @@
+const content = document.querySelectorAll(".faq-content");
+const icon = document.querySelectorAll(".faq-icon");
+
+icon.forEach((button) => {
+  button.addEventListener("click", function () {});
+});
+
+content.forEach((el) => {
+  el.classList.add("hidden");
+});
