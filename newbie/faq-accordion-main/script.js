4ceb7948f76acb6c4d1cd09bc840a994a41d3865
kilozdazolik <paulsonzoltan@protonmail.com>
2025-02-09 12:55:19 +0100

    - Refactor the JavaScript code to improve the FAQ accordion functionality. - Add responsive styles for the FAQ accordion on different screen sizes.

--- newbie/faq-accordion-main/script.js
+++ newbie/faq-accordion-main/script.js
@@ -1,8 +1,15 @@
-const content = document.querySelectorAll(".faq-content");
 const icon = document.querySelectorAll(".faq-icon");
+const content = document.querySelectorAll(".faq-content");
 
-icon.forEach((button) => {
-  button.addEventListener("click", function () {});
+icon.forEach((el, index) => {
+  el.addEventListener("click", () => {
+    content[index].classList.toggle("hidden");
+    if (content[index].classList.contains("hidden")) {
+      el.src = "assets/images/icon-plus.svg";
+    } else {
+      el.src = "assets/images/icon-minus.svg";
+    }
+  });
 });
 
 content.forEach((el) => {
